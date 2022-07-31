3174ad710f6f3990d015b2be5ebad4cdb7235f53
kocerenes <eneskocerr41@gmail.com>
2022-07-31 11:24:08 +0300

    Initial commit

---
+++ app/src/main/java/com/enesk/corestock/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.enesk.corestock
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.enesk.corestock.ui.theme.CoreStockTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CoreStockTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CoreStockTheme {
+        Greeting("Android")
+    }
+}
